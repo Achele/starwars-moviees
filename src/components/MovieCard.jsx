39d085b2d919ae91acdf94aee1ed8c38536a1eba
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-05-09 19:49:24 +0100

    Refactor Api fetch logic

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,23 @@
+import { Link } from "react-router-dom";
 import dateFormat from "./dateFormat";
 
-const MovieCard = (props) => {
+const MovieCard = (movie, actor) => {
+  console.log(`results: ============= ${movie}`);
   return (
     <div className="card">
       <div className="card-info">
-        <h2>{props.title}</h2>
-        <p className="release-date">{dateFormat(props.release_date)}</p>
+        <h2>{movie.title}</h2>
+        <p className="release-date">{dateFormat(movie.release_date)}</p>
 
-        <p className="summary">{props.opening_crawl}</p>
-        <hr className="line-break" />
-        <a href="#">More Info</a>
+        <p className="summary">{movie.opening_crawl.substring(0, 260)}...</p>
+
+        <Link
+          to={`/:${movie.title}`}
+          state={{ movie: movie, actor: actor }}
+          className="movie-link"
+        >
+          More info
+        </Link>
       </div>
     </div>
   );
